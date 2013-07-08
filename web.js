3820d9f678a6dce0c5d6ced98f8586391bad57e5
Alberto Demetrio <albertodemetrio@ubuntu.(none)>
2013-07-08 21:48:53 +0200

    Added index.html

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var name = fs.readFileSync('index.html');
+
+var buffer = buf.toString('name');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('buffer');
 });
 
 var port = process.env.PORT || 5000;
