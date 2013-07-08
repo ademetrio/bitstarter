830326d11c1db82b01d8f1371a8f3a445bebd59f
Alberto Demetrio <albertodemetrio@ubuntu.(none)>
2013-07-08 22:10:05 +0200

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync('index.html');
+var buffer = new Buffer (256)
 
-var string = buffer.toString();
+var file = fs.readFileSync('index.html');
+
+var lit = buffer.write(file); 
+
+var string = lit.toSring();
 
 app.get('/', function(request, response) {
   response.send('string');
